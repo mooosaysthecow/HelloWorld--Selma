1f35824711b203ea8d8cea6a40ecf22cc035be96
mooosaysthecow <selma.h99@live.se>
2017-01-19 13:52:32 +0100

    AddedCode

---
+++ HelloWorld!-Selma/Texting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld__Selma
+{
+    class Texting
+    {
+
+
+        public void Run()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+
+    }
+}
